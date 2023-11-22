22ade1659a8b0302bac0bd7f2f60a1142fe9a493
Jade-test <133740529+hhnmsDeveloper@users.noreply.github.com>
2023-11-22 15:23:03 +0900

    update: ending

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomeView from "./views/HomeView";
+import lenis from "./utils/lenis";
+import link from "./utils/link";
 
 const App = () => {
+    useEffect(() => {
+        lenis();
+        link();
+    }, []);
+
     return (
     <BrowserRouter>
         <Routes>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+
+import { footerText } from "../constants";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer id="footer" role="contentinfo">
+            <div className="footer__inner">
+                <div className="footer__text">
+                    <span>webstoryboy</span>
+                    <span>© webs</span>
+                </div>
+                <div className="footer__info">
+                    <div className="left">
+                        <div className="title">
+                            <a href="/">sign up</a>
+                        </div>
+                        <p>회원가입을 하시면 댓글과 게시판 기능을 이용할 수 있습니다.</p>
+                    </div>
+                    <div className="right">
+                        <h3>social</h3>
+                        <ul>
+                            {footerText.map((footer, key) => (
+                                <li key={key}>
+                                    <a href={footer.link}>{footer.title}</a>
+                                    <em>{footer.desc}</em>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer__right">
+                    © 2023 webstoryboy<br />
+                    이 사이트는 리액트를 이용하여 제작하였습니다.
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
